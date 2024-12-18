caf67fad8a7229706182d52273e36ecc5bb024b2
agung harwin n <agungharwinn@gmail.com>
2024-12-18 17:01:28 +0700

    create Hero and About

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AnimatedTitle = ({ title, containerClass }) => {
+  return (
+    <div className={`animated-title ${containerClass}`}>
+      {title.split("<br />").map((line, i) => (
+        <div
+          key={i}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
