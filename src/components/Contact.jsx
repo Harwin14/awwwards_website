dbe718ff0a66ce4351b325440d196569503d96ad
agung harwin n <agungharwinn@gmail.com>
2024-12-23 14:02:03 +0700

    add story

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id="contact" className='my-20 min-h-96 w-screen px-10'>
+      <div className="relative rounded-lg bg-black py-24"></div>
+    </div>
+  )
+}
+
+export default Contact
